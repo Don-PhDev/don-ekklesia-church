2538d14214c0204ba7096df93f69f7dd9d7b623b
Don Forrest <forrest@philippinedev.com>
2022-05-27 23:53:19 +0800

    Add test on Post model validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Post, type: :model do
   describe "relationships" do
     it { should belong_to(:user) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:content) }
+  end
 end
