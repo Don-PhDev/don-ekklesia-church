855f2ecf81367da3abf1f2b10341532ab9ab95bb
Don Forrest <forrest@philippinedev.com>
2022-05-29 23:55:33 +0800

    Add validation test on model Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,8 +5,37 @@ RSpec.describe Post, type: :model do
     it { should belong_to(:user) }
   end
 
-  describe 'validations' do
+  describe "validations" do
+    let(:user) { build(:user) }
+
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:content) }
+
+    it "is not valid without a title" do
+      post = Post.new(title: nil)
+      expect(post).to_not be_valid
+    end
+
+    subject {
+      described_class.new(
+        title: "Anything",
+        content: "Lorem ipsum",
+        user: user
+      )
+    }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a title" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a content" do
+      subject.content = nil
+      expect(subject).to_not be_valid
+    end
   end
 end
