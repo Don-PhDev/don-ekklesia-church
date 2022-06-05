074fce1ffedd7722c094578fc9d56e3344e62752
Don Forrest <forrest@philippinedev.com>
2022-06-05 20:37:43 +0800

    Add test on Posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe PostsController, :type => :controller do
       expect(response.status).to eq(200)
     end
   end
+
+  context "without render_views" do
+    describe "GET index" do
+      it "renders the RSpec generated template" do
+        get :index
+        expect(response.body).to eq("")
+      end
+    end
+  end
 end
